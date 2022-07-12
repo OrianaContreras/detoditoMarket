e500e590ee38272c1142c70288739e3fd425b1d2
Oriana Contreras <ocontreras@ecomsur.com>
2022-07-12 00:34:26 -0400

    Avances

--- js/main.js
+++ js/main.js
@@ -26,3 +26,74 @@ $(document).ready(function() {
         }
     })
 })
+
+const templateCarrito = document.getElementById('template-carrito').content
+let carrito = [];
+const mostrarCarrito = () => {
+        
+    items.innerHTML ='';
+    Object.values(carrito).forEach(productoSeleccionado => {
+    templateCarrito.querySelector('th').textContent = productoSeleccionado.index;
+    templateCarrito.querySelectorAll('td')[0].textContent = productoSeleccionado.title;
+    templateCarrito.querySelectorAll('td')[1].textContent = productoSeleccionado.cantidad;
+    templateCarrito.querySelector('.btn-info').dataset.index = productoSeleccionado.index;
+    templateCarrito.querySelector('.btn-danger').dataset.index = productoSeleccionado.index;
+    console.log(templateCarrito.querySelector('.btn-info').dataset.index = productoSeleccionado.index)
+
+
+        const clone = templateCarrito.cloneNode(true);
+        fragment.appendChild(clone);
+    })
+
+    items.appendChild(fragment)
+
+    footerTabla()
+}
+    const footerTabla = () =>{
+        footer.innerHTML = '';
+        if(Object.keys(carrito).length === 0){
+            footer.innerHTML=`
+            <th scope="row" colspan="5">Carrito vacío</th>
+            `
+        return 
+        }
+        const cantidadDeProductosAgregados = Object.values(carrito).reduce((acc,{cantidad}) => acc + cantidad,0)
+        templateFooter.querySelectorAll('td')[0].textContent = cantidadDeProductosAgregados
+        const clone = templateFooter.cloneNode(true) 
+        fragment.appendChild(clone)
+        footer.appendChild(fragment)
+
+        const btnVaciarCarrito = document.getElementById('vaciar-carrito')
+        btnVaciarCarrito.addEventListener('click',() => {
+            carrito = {}
+        mostrarCarrito()
+
+        })   
+    }
+
+items.addEventListener('click', element => { btnAumentarDisminuir(element) })
+
+let btnAumentarDisminuir = element => {
+   
+    if(element.target.className == 'btn-info'){
+        console.log('estoy dentro del if')
+    let producto = carrito[element.target.dataset.index]
+    producto.cantidad = producto.cantidad + 1
+    console.log(producto)
+    carrito[element.target.dataset.index] = {...producto}
+    mostrarCarrito()
+    }
+
+    if (element.target.className == 'btn-danger'){
+        
+        let producto = carrito[element.target.dataset.index]
+        producto.cantidad = producto.cantidad - 1
+        if (producto.cantidad === 0) {
+            delete carrito[element.target.dataset.index]
+        } else {
+            carrito[element.target.dataset.index] = {...producto}
+        }
+        mostrarCarrito()
+    }
+
+}
